f42ca83727381ed9ec0ea0989def9bc98a01213d
Joe Germuska <joe@germuska.com>
2009-12-13 12:47:04 -0600

    very rough urls.py

--- chitif/expenditures/views.py
+++ chitif/expenditures/views.py
@@ -1,1 +1,10 @@
+from django.views.decorators.cache import cache_page
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template.loader import render_to_string
+from django.contrib.gis.shortcuts import render_to_kml
+from django.http import HttpResponse
+from django.conf import settings
 # Create your views here.
+
+def home(request):
+    return render_to_response("expenditures/index.html", {})
